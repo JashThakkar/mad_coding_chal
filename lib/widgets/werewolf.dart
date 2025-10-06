c52c3fa91662ded544ff41297d3b51d1c5e486cc
unknown <zarahgats@gmail.com>
2025-10-06 18:34:33 -0400

    added werewolf widget w static image

---
+++ lib/widgets/werewolf.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Werewolf extends StatefulWidget {
+  const Werewolf({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Image.asset('assets/images/werewolf.png', width: 200));
+  }
+}
